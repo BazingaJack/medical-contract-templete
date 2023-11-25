9bb14b8c4376af68e47cb14b447670a35a4314c8
BazingaJack <634283136@qq.com>
2023-11-25 18:05:48 +0800

    add more functions

--- contracts/MainContract.sol
+++ contracts/MainContract.sol
@@ -12,20 +12,36 @@ contract MainContract is AccessControlDefaultAdminRules{
     bytes32 public constant DOCTOR_ROLE = keccak256("DOCTOR");//doctor
     bytes32 public constant PHARMACY_DEPARTMENT_ROLE = keccak256("PHARMACY_DEPARTMENT");//pharmacy department
     uint256 public nextRoleId;
-    uint256 public nextRecordId;
     
-    struct healthData{
+    struct basicHealthData{
         uint256 height;
         uint256 weight;
         uint256 bloodPressure;
         uint256 bloodSugar;
     }
 
+    struct medicalRecord{
+        uint256 recordId;
+        uint256 time;
+        string diseaseName;
+        string detailInfo;
+        string doctorAdvice;
+        bool isValid;
+    }
+
+    struct reservationRecord {
+        address patientAddr;
+        address doctorAddr;
+        uint256 time;
+        bool isValid;
+    }
+
     struct patientData {
         string name;
         string idNumber;
         string phoneNumber;
-        healthData patientHealthInfo;
+        uint256 medicalRecordNum;
+        basicHealthData patientHealthInfo;
         bool isValid;
     }
 
@@ -38,17 +54,12 @@ contract MainContract is AccessControlDefaultAdminRules{
         bool isValid;
     }
 
-    struct record {
-        address patientAddr;
-        address doctorAddr;
-        uint256 time;
-    }
-
     mapping(uint256 => bytes32) roles;
     mapping(address => patientData) patientDataSet;
     mapping(address => address) approveInfo;
     mapping(address => doctorData) doctorDataSet;
-    mapping(uint256 => record) records;
+    mapping(address => reservationRecord) reservationRecords;
+    mapping(address => mapping(uint256 => medicalRecord)) medicalRecords;
 
     constructor()AccessControlDefaultAdminRules(3 days,msg.sender){
         roles[0] = MEDICAL_INSTITUTION_ROLE;
@@ -56,7 +67,6 @@ contract MainContract is AccessControlDefaultAdminRules{
         roles[2] = DOCTOR_ROLE;
         roles[3] = PHARMACY_DEPARTMENT_ROLE;
         nextRoleId = 4;
-        nextRecordId = 0;
     }
 
     function checkRole(address _checkAddr,bytes32 _role) public view onlyRole(DEFAULT_ADMIN_ROLE) returns(bool) {
@@ -66,29 +76,13 @@ contract MainContract is AccessControlDefaultAdminRules{
     //Medical Institution relevant function
     
     //Patient relevant function
-    function addPatient(
-        address _patientAddr,
-        string memory _name,
-        string memory _idNumber,
-        string memory _phoneNumber) public onlyRole(DEFAULT_ADMIN_ROLE){
+    function addPatient(address _patientAddr,string memory _name,string memory _idNumber,string memory _phoneNumber) 
+    public onlyRole(DEFAULT_ADMIN_ROLE){
             require(patientDataSet[_patientAddr].isValid == false,"Error : This address has already been added as a patient.");
-            healthData memory newHealthData = healthData(0,0,0,0);
-            patientData memory newPatientData = patientData(_name,_idNumber,_phoneNumber,newHealthData,true);
+            basicHealthData memory newHealthData = basicHealthData(0,0,0,0);
+            patientData memory newPatientData = patientData(_name,_idNumber,_phoneNumber,0,newHealthData,true);
             patientDataSet[_patientAddr] = newPatientData;
             grantRole(PATIENT_ROLE, _patientAddr);
-        }
-
-    function updatePatientHealthInfo(
-        address _patientAddr,
-        uint256 _height,
-        uint256 _weight,
-        uint256 _bloodPressure,
-        uint256 _bloodSugar) public onlyRole(DEFAULT_ADMIN_ROLE){
-            require(patientDataSet[_patientAddr].isValid == true,"Error : This patient hasn't been added yet.");
-            patientDataSet[_patientAddr].patientHealthInfo.height = _height;
-            patientDataSet[_patientAddr].patientHealthInfo.weight = _weight;
-            patientDataSet[_patientAddr].patientHealthInfo.bloodPressure = _bloodPressure;
-            patientDataSet[_patientAddr].patientHealthInfo.bloodSugar = _bloodSugar;
         }
 
     function approveToDoctor(address _doctorAddr) public onlyRole(PATIENT_ROLE) {
@@ -97,12 +91,12 @@ contract MainContract is AccessControlDefaultAdminRules{
         approveInfo[msg.sender] = _doctorAddr;
     }
 
-    function makeAnAppointment(address _doctorAddr,uint256 _time) public onlyRole(PATIENT_ROLE) {
+    function makeReservation(address _doctorAddr,uint256 _time) public onlyRole(PATIENT_ROLE) {
         require(hasRole(DOCTOR_ROLE, _doctorAddr),"Error : Health data can only be approved to doctor.");
         require(doctorDataSet[_doctorAddr].isAvaliable == true,"Error : This doctor is not avaliable");
         doctorDataSet[_doctorAddr].isAvaliable = false;
-        record memory r = record(msg.sender,_doctorAddr,_time);
-        records[nextRecordId++] = r;
+        reservationRecord memory r = reservationRecord(msg.sender,_doctorAddr,_time,true);
+        reservationRecords[msg.sender] = r;
     }
 
     //Doctor relevant function
@@ -113,6 +107,40 @@ contract MainContract is AccessControlDefaultAdminRules{
         doctorDataSet[_doctorAddr] = newDoctorData;
         grantRole(DOCTOR_ROLE, _doctorAddr);
     }
+
+    function getPatientMedicalRecord(address _patientAddr,uint256 _recordId)
+    public view onlyRole(DOCTOR_ROLE) returns(uint256,string memory,string memory,string memory){
+        require(approveInfo[_patientAddr] == msg.sender,"Error : You have no access to patient's medical record.");
+        require(patientDataSet[_patientAddr].medicalRecordNum > _recordId,"Error : illegal record id.");
+        medicalRecord memory r = medicalRecords[_patientAddr][_recordId];
+        return(r.time,r.diseaseName,r.detailInfo,r.doctorAdvice);
+    }
+
+    function updatePatientBasicHealthInfo(address _patientAddr,uint256 _height,uint256 _weight,uint256 _bloodPressure,uint256 _bloodSugar) 
+    internal onlyRole(DOCTOR_ROLE){
+            require(patientDataSet[_patientAddr].isValid == true,"Error : This patient hasn't been added yet.");
+            patientDataSet[_patientAddr].patientHealthInfo.height = _height;
+            patientDataSet[_patientAddr].patientHealthInfo.weight = _weight;
+            patientDataSet[_patientAddr].patientHealthInfo.bloodPressure = _bloodPressure;
+            patientDataSet[_patientAddr].patientHealthInfo.bloodSugar = _bloodSugar;
+        }
+
+    function updatePatientMedicalRecord(address _patientAddr,uint256 _time,
+    string memory _diseaseName,string memory _detailInfo,string memory _doctorAdvice)
+    internal onlyRole(DOCTOR_ROLE){
+        require(patientDataSet[_patientAddr].isValid == true,"Error : This patient hasn't been added yet.");
+        uint256 nextRecordId = patientDataSet[_patientAddr].medicalRecordNum;
+        patientDataSet[_patientAddr].medicalRecordNum++;
+        medicalRecord memory r = medicalRecord(nextRecordId,_time,_diseaseName,_detailInfo,_doctorAdvice,true);
+        medicalRecords[_patientAddr][nextRecordId] = r;
+    }
     
+    function examineAndUpdate(address _patientAddr,basicHealthData memory _examineData,medicalRecord memory _medicalData)
+    public onlyRole(DOCTOR_ROLE){
+        require(patientDataSet[_patientAddr].isValid == true,"Error : This patient hasn't been added yet.");
+        require(reservationRecords[_patientAddr].isValid == true,"Error : Reservation information doesn't exist.");
+        updatePatientBasicHealthInfo(_patientAddr, _examineData.height, _examineData.weight, _examineData.bloodPressure, _examineData.bloodSugar);
+        updatePatientMedicalRecord(_patientAddr, _medicalData.time, _medicalData.diseaseName, _medicalData.detailInfo, _medicalData.doctorAdvice);
+    }
     //Pharmacy department relevant function
 }
